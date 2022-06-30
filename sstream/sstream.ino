a4e6c06954ddcce9af8dc2d441d3484cc45b5563
CaseyNelson314 <caseynelson314@gmail.com>
2022-06-30 22:48:56 +0900

    fix sample program

--- sstream/sstream.ino
+++ sstream/sstream.ino
@@ -1,18 +1,10 @@
 #include "sstream.h"
 #include "Sample.h"
 
-Sample samp(4321);
-
 void setup() {
   Serial.begin(115200);
-  const uint32_t t = micros();
-
-  std::sout << samp << std::endl;
-
-  std::sout << "now : " << 1000 << std::tab << "ms" << std::endl;
-
-  std::sout << micros() - t << std::endl;
 }
 
 void loop() {
+  std::sout << "now : " << millis() << std::tab << "ms" << std::endl;
 }

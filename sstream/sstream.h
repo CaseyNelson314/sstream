fe41600eed8807a52754415d1368ffee9e3a4d0f
Casey_Nelson <91818705+CaseyNelson314@users.noreply.github.com>
2022-08-11 15:15:53 +0900

    インライン化

--- sstream/sstream.h
+++ sstream/sstream.h
@@ -7,56 +7,55 @@
 #pragma once
 
 namespace std {
-  
+
 String streambuf;  // stream buffer
 
 /**
    @breif オプション挿入
 */
-void flush() {
+inline void flush() {
   streambuf = "";  // baffer clear
 }
-void endl() {
+inline void endl() {
   Serial.println(streambuf);
   flush();
 }
-void end() {
+inline void end() {
   Serial.print(streambuf);
   flush();
 }
-void tab() {
+inline void tab() {
   streambuf += '\t';  // stream挿入
 }
-  
 
 class sstream {
-  public:
-    /**
-       @brief ストリーム挿入
-       @param 出力値
-    */
-    template<class T>
-    friend sstream& operator<<(sstream &sstrm, const T input) noexcept {
-      streambuf += input;
-      return sstrm;
-    }
-    friend sstream& operator<<(sstream &sstrm, void(*input)()) noexcept {
-      input();
-      return sstrm;
-    }
+ public:
+  /**
+     @brief ストリーム挿入
+     @param 出力値
+  */
+  template <class T>
+  friend sstream& operator<<(sstream& sstrm, const T input) noexcept {
+    streambuf += input;
+    return sstrm;
+  }
+  friend sstream& operator<<(sstream& sstrm, void (*other)()) noexcept {
+    other();
+    return sstrm;
+  }
 
-    /**
-       @brief 通常出力
-       @param 出力値
-    */
-    template<class T>
-    void print(const T input) const noexcept {
-      Serial.print(input);
-    }
-    template<class T>
-    void println(const T input) const noexcept {
-      Serial.println(input);
-    }
+  /**
+     @brief 通常出力
+     @param 出力値
+  */
+  template <class T>
+  void print(const T input) const noexcept {
+    Serial.print(input);
+  }
+  template <class T>
+  void println(const T input) const noexcept {
+    Serial.println(input);
+  }
 };
 
 extern sstream sout;
